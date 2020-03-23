5d7cea7176db403253ec915ce8bf3d5297dbc5c2
Daegudude <k3hppk@gmail.com>
2020-03-23 14:58:01 +0900

    Man! Upload the shit

--- test.js
+++ test.js
@@ -1,38 +1,21 @@
-const add = (numOne, numTwo) => {
-  return numOne + numTwo;
-};
+let operators = ['x', '-', '-', '+', '÷'];
 
-const subtract = (numOne, numTwo) => {
-  return numOne - numTwo;
-};
+function isMultiplicationOrDivision(operator) {
+  return (operator == 'x' || operator == '÷') ? true : false;
+}
 
-const multiply = (numOne, numTwo) => {
-  return numOne * numTwo;
-};
-
-const divide = (numOne, numTwo) => {
-  return numOne / numTwo;
-};
-
-// Create a new function operate that takes an operator and 2 numbers and then
-// calls one of the above functions on the numbers
-
-const operate = (operator, numOne, numTwo) => {
-  if (operator === '+') {
-    return add(numOne, numTwo);
-  } 
-  else if (operator === '-') {
-    return subtract(numOne, numTwo);
-  } 
-  else if (operator === '*') {
-    return multiply(numOne, numTwo);
-  } 
-  else if (operator === '/')  {
-    return divide(numOne, numTwo);
+function findIndexMultiplicationAndDivision(array1) {
+  let indexLocations = []
+  
+  for (let i = 0; i < array1.length; i++) {
+    
+    if(isMultiplicationOrDivision(array1[i])) {
+      console.log(`satisfied: ${array1[i]}, ${i}`);
+      indexLocations.push(i);
+    } 
   }
-};
 
-console.log(operate('-', 5, 10));
-console.log(operate('+', 5, 10));
-console.log(operate('*', 5, 10));
-console.log(operate('/', 5, 10));
+  return indexLocations;
+}
+
+console.log(findIndexMultiplicationAndDivision(operators));
